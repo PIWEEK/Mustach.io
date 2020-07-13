82433c6e89d4628bd6ca594305cec75032c7c9d2
Samuel Gomez <samuel.gomez@kaleidos.net>
2020-07-13 19:09:40 +0200

    #004 add animation menu

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -6,12 +6,16 @@ const Dropdown = () => {
   const [option, setOption] = useState('Face');
   const [visible, setVisible] = useState(false);
   const options = ['Face', 'Hairstyle', 'Fringe', 'Eyes', 'Eyebrows', 'Glases', 'Nose', 'Mouth', 'Neck', 'Accesories'];
+  const selectOption = (opcion)=> {
+    setOption(opcion);
+    setVisible(!visible);
+  }
 
   return <div className={`dropdown-container ${visible?'visible':''}`}>
-    <h2 onClick={()=>setVisible(!visible)}>{option}</h2>
+    <h2 onClick={()=>setVisible(!visible)}>{option} </h2>
     <ul className='options-list'>
       {
-        options.map(x=><li className={`option ${x===option?'selected': ''}`} onClick={()=>setOption(x)}>{x}</li>)
+        options.map(x=><li className={`option ${x===option?'selected': ''}`} onClick={()=>selectOption(x)}>{x}</li>)
       }
     </ul>
   </div>
