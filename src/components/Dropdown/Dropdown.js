a71de7abd647fe87ca1c286ce91dba0715cb836a
maria.cano <maria.cano@kaleidos.net>
2020-12-14 10:19:20 +0100

    '#042 Dropdown menu arrow'

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -10,7 +10,9 @@ const Dropdown = ({optiosMenu, option, setOption}) => {
   }
 
   return <div className={`dropdown-container ${visible?'visible':''}`}>
-    <h2 onClick={()=>setVisible(!visible)}>{option} </h2>
+    <h2 onClick={()=>setVisible(!visible)}>{option}
+      <svg class="dropdown-arrow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 31 16"><defs/><path fill="#000" d="M29.9.5l-.4-.4-14 13-14-13-.4.4L15.5 16 29.9.5z"/></svg>
+    </h2>
     <ul className='options-list'>
       {
         optiosMenu.map((x, index)=><li key={index} className={`option ${x===option?'selected': ''}`} onClick={()=>selectOption(x)}>{x}</li>)
