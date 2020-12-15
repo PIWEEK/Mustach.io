02a49ff3aaf816d9f9f8e8388ff4d0383ea33033
maria.cano <maria.cano@kaleidos.net>
2020-12-15 15:34:03 +0100

    #043 Responsive layout

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -11,7 +11,7 @@ const Dropdown = ({optiosMenu, option, setOption}) => {
 
   return <div className={`dropdown-container ${visible?'visible':''}`}>
     <h2 onClick={()=>setVisible(!visible)}>{option}
-      <svg class="dropdown-arrow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 31 16"><defs/><path fill="#000" d="M29.9.5l-.4-.4-14 13-14-13-.4.4L15.5 16 29.9.5z"/></svg>
+      <svg className="dropdown-arrow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 31 16"><defs/><path fill="#000" d="M29.9.5l-.4-.4-14 13-14-13-.4.4L15.5 16 29.9.5z"/></svg>
     </h2>
     <ul className='options-list'>
       {
