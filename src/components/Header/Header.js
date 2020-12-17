c59bebac2e097c0121554b138d773c94e3a6615c
Ester Latorre <ester.latorre@kaleidos.net>
2020-12-17 20:43:22 +0100

    wip

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   }
 
   return <header className="App-header">
-    <h1>Mustachio</h1>
+    <a href="/"><h1>Mustachio</h1></a>
     <div onClick={()=>setVisible(!visible)} className={`download-button ${visible?'visible':''}`}>
       <svg className="download-arrow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 40 62"><defs/><path fill="#000" d="M20 .3L21.7 54l18-15.6L20 61.4.3 38.4l18 15.7L20 .3z"/></svg>
       <span className="download-text">Download</span>
